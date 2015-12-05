2472fa78ce3dc07b5c34ac9097ff8ab087dc609b
Ricardo Silverio de Souza <ricardo.silverio.souza@gmail.com>
2015-12-05 17:53:26 -0200

    Ajustes em HTMLFile Service
    Ajustes em HTMLFile Service

--- 99News/HTMLFileService.swift
+++ 99News/HTMLFileService.swift
@@ -10,18 +10,31 @@ import UIKit
 
 class HTMLFileService: NSObject {
     
-    let pathPaginaHTML:String
+    private let pathPaginaHTML:String
+    private let urlArquivo:NSURL
     
     override init() {
         let path = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
         let docPath = path[0]
         pathPaginaHTML = docPath + "/" + "articlePage.html"
-        print(pathPaginaHTML)
+        urlArquivo = NSURL(fileURLWithPath: self.pathPaginaHTML)
     }
     
     func escreverArquivo(conteudo:String) {
         let data = conteudo.dataUsingEncoding(NSUTF8StringEncoding)!
         NSFileManager.defaultManager().createFileAtPath(pathPaginaHTML, contents: data, attributes: nil)
     }
+    
+    func getURLArquivo() -> NSURL {
+        return urlArquivo
+    }
+    
+    /* Exemplo de uso em webview
+
+        htmlService?.escreverArquivo(noticia.resumo)
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+        let request = NSURLRequest(URL: self.htmlService!.getURLArquivo())
+        self.webView.loadRequest(request)
+    */
 
 }
